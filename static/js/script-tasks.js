0ede1d12de057f06723adc099f9cb70e9b7602df
Zhukov531 <zhukov531@gmail.com>
2024-08-09 22:58:59 +0300

    tasks

---
+++ static/js/script-tasks.js
@@ -0,0 +1,14 @@
+document.getElementById("daily-tasks-btn").addEventListener("click", function() {
+    document.getElementById("daily-quests").classList.remove("hidden");
+});
+
+document.getElementById("close-tasks-btn").addEventListener("click", function() {
+    document.getElementById("daily-quests").classList.add("hidden");
+});
+document.getElementById("daily-tasks-btn").addEventListener("click", function() {
+    document.getElementById("daily-quests").classList.remove("hidden");
+});
+
+document.getElementById("close-tasks-btn").addEventListener("click", function() {
+    document.getElementById("daily-quests").classList.add("hidden");
+});
